34e0686aa4485268d2f27aab3ebad9043cf8fcc6
zjbman <zjbman@sina.com>
2018-03-27 20:50:41 +0800

    shiro的简单实现步骤说明及相关配置解释

--- src/main/java/com/paper/controller/LoginController.java
+++ src/main/java/com/paper/controller/LoginController.java
@@ -1,11 +1,9 @@
 package com.paper.controller;
 
 import com.paper.service.AccountService;
+import com.paper.util.MD5;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.ExcessiveAttemptsException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,36 +24,51 @@ public class LoginController {
 
 
     @RequestMapping(value = "/Login", method = RequestMethod.POST)
-    public String Login(@RequestParam("username") String username, @RequestParam("password") String password, Model model) {
-        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+    public String Login(@RequestParam("username") String username,
+                        @RequestParam("password") String password, Model model) {
+        /* 对密码进行32位小写md5*/
+        UsernamePasswordToken token = new UsernamePasswordToken(username, MD5.get32MD5(password));
         Subject subject = SecurityUtils.getSubject();
-
         try {
             subject.login(token);
+
         } catch (IncorrectCredentialsException ice) {
             // 捕获密码错误异常
-            ModelAndView mv = new ModelAndView("error/404");
-            mv.addObject("message", "password error!");
-//            return mv;
+            System.out.println("账户密码 " + token.getPrincipal()  + " 不正确!");
+
         } catch (UnknownAccountException uae) {
             // 捕获未知用户名异常
-            ModelAndView mv = new ModelAndView("error/404");
-            mv.addObject("message", "username error!");
-//            return mv;
+            System.out.println("用户名不存在:" + token.getPrincipal());
+
         } catch (ExcessiveAttemptsException eae) {
             // 捕获错误登录过多的异常
-            ModelAndView mv = new ModelAndView("error/404");
-            mv.addObject("message", "times error");
-//            return mv;
+            System.out.println("用户名 " + token.getPrincipal() + " 被锁定 !");
+
+        } catch (LockedAccountException lae) {
+            System.out.println("用户名 " + token.getPrincipal() + " 被锁定 !");
         }
+
+
+        if(subject.isAuthenticated()){
+            /* 认证成功后*/
+            System.out.println("用户 " + subject.getPrincipal() + " 登陆成功！");
+
+            //测试角色
+            System.out.println("是否拥有 manager 角色：" + subject.hasRole("manager"));
+
+            //测试权限
+            System.out.println("是否拥有 user:create 权限" + subject.isPermitted("user:create"));
+
+        }else{
+            System.out.println("用户 " + username + "， token无效");
+        }
+
+
+
 
         System.out.println("username == " + username);
         System.out.println("password == " + password);
 
-        accountService.findAll("");
-
-
-//        return new ModelAndView("main");
         return "main";
     }
 
--- src/main/java/com/paper/shiro/MyRealm.java
+++ src/main/java/com/paper/shiro/MyRealm.java
@@ -3,27 +3,103 @@ package com.paper.shiro;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author zjbman
- * @Description
+ *
+ * @Description 域。Shiro 从 Realm 获取安全数据（如用户、角色、权限），
+ *  就是说 SecurityManager 要验证用户身份，那么它需要从 Realm 获取
+ *  相应的用户进行比较以确定用户身份是否合法，也需要从 Realm 得到
+ *  用户相应的角色/权限进行验证用户是否能进行操作。我们可以把 Realm 看成 DataSource，即安全数据源。
+ *  它可以是 JDBC 实现，也可以是 LDAP 实现，或者内存实现等。
+ *
  * @date 2018/3/22 17:14
  **/
 public class MyRealm extends AuthorizingRealm {
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(
-            PrincipalCollection principals) {
-        return null;
-    }
 
+    /**
+     * 认证
+     * @param token
+     * @return
+     * @throws AuthenticationException
+     */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(
             AuthenticationToken token) throws AuthenticationException {
-        return null;
+
+        /* 1.从token中获取用户身份信息*/
+        String username = (String)token.getPrincipal();
+
+        /* 2.通过username查询数据库*/
+
+
+        /* 3.如果查询不到则返回null*/
+
+
+        /* 4.获取从数据库查询出来的用户密码*/
+        String password = null;
+
+        /* 5.返回认证信息由父类AuthenticatingRealm进行认证*/
+        return new SimpleAuthenticationInfo(username, password, getName());
     }
+
+
+    /**
+     * 授权
+     *
+     * 逻辑：用户通过认证后，才进行授权操作，
+     * 所以这个方法的参数直接就是principals（包含当前用户信息）
+     * @param principals
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(
+            PrincipalCollection principals) {
+
+        /* 1.获取身份信息*/
+        String username = (String)principals.getPrimaryPrincipal();
+
+        /* 2.根据身份信息从数据库中查询权限数据*/
+        // 这里使用静态数据模拟
+        List<String> permissions = new ArrayList<String>();
+        permissions.add("user:*");
+        permissions.add("department:*");
+
+        /* 3.将权限信息封闭为AuthorizationInfo*/
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        //模拟数据，添加manager角色
+        info.addRole("manager");
+
+        for(String permission:permissions){
+            info.addStringPermission(permission);
+        }
+
+
+        /* 4.返回AuthorizationInfo*/
+        return info;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /**
      * 为当前登录的Subject授予角色和权限
      * @see  经测试:本例中该方法的调用时机为需授权资源被访问时
