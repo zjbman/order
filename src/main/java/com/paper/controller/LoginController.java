bf1aa6cf4281f3f28b606bb7c62baf833a6871f6
zjbman <zjbman@sina.com>
2018-04-27 17:58:57 +0800

    用Logger替代System.out.println

--- src/main/java/com/paper/controller/LoginController.java
+++ src/main/java/com/paper/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.paper.controller;
 
 import com.paper.config.WebParam;
 import com.paper.util.MD5;
+import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.session.Session;
@@ -19,6 +20,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class LoginController {
 
+    private Logger logger = Logger.getLogger(LoginController.class.getSimpleName());
+
     @RequestMapping(value = "/Login", method = RequestMethod.POST)
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password, Model model) {
@@ -29,34 +32,28 @@ public class LoginController {
             subject.login(token);
             if (subject.isAuthenticated()) {
             /* 认证成功后*/
-                System.out.println("用户 " + subject.getPrincipal() + " 登陆成功！");
+                logger.info("用户 " + subject.getPrincipal() + " 登陆成功！");
 
             /* 判断当前用户什么角色*/
                 if (subject.hasRole("管理员")) {
-                    System.out.println("用户 " + subject.getPrincipal() + " 角色是 管理员！");
-
+                    logger.info("用户 " + subject.getPrincipal() + " 角色是 管理员！");
                 } else if (subject.hasRole("商家")) {
-                    System.out.println("用户 " + subject.getPrincipal() + " 角色是 商家！");
-
+                    logger.info("用户 " + subject.getPrincipal() + " 角色是 商家！");
                 } else if (subject.hasRole("开发测试")) {
-                    System.out.println("用户 " + subject.getPrincipal() + " 角色是 开发测试！");
-
+                    logger.info("用户 " + subject.getPrincipal() + " 角色是 开发测试！");
                 } else {
-                    System.out.println("用户 " + subject.getPrincipal() + " 角色是 未知！");
+                    logger.info("用户 " + subject.getPrincipal() + " 角色是 未知！");
                 }
 
             /* 判断当前用户什么权限*/
                 if (subject.isPermitted("管理员权限")) {
-                    System.out.println("用户 " + subject.getPrincipal() + " 权限是 管理员权限！");
-
+                    logger.info("用户 " + subject.getPrincipal() + " 权限是 管理员权限！");
                 } else if (subject.isPermitted("商家权限")) {
-                    System.out.println("用户 " + subject.getPrincipal() + " 权限是 商家权限！");
-
+                    logger.info("用户 " + subject.getPrincipal() + " 权限是 商家权限！");
                 } else if (subject.isPermitted("消费者权限")) {
-                    System.out.println("用户 " + subject.getPrincipal() + " 权限是 消费者权限！");
-
+                    logger.info("用户 " + subject.getPrincipal() + " 权限是 消费者权限！");
                 } else {
-                    System.out.println("用户 " + subject.getPrincipal() + " 权限 未知！");
+                    logger.info("用户 " + subject.getPrincipal() + " 权限 未知！");
                 }
 
             /* 成功跳转到主界面*/
@@ -69,18 +66,15 @@ public class LoginController {
 
         } catch (IncorrectCredentialsException ice) {
             // 捕获密码错误异常
-            System.out.println("账户密码 " + token.getPrincipal() + " 不正确!");
-
+            logger.info("账户密码 " + token.getPrincipal() + " 不正确!");
         } catch (UnknownAccountException uae) {
             // 捕获未知用户名异常
-            System.out.println("用户名不存在:" + token.getPrincipal());
-
+            logger.info("用户名不存在:" + token.getPrincipal());
         } catch (ExcessiveAttemptsException eae) {
             // 捕获错误登录过多的异常
-            System.out.println("用户名 " + token.getPrincipal() + " 错误登录过多 !");
-
+            logger.info("用户名 " + token.getPrincipal() + " 错误登录过多 !");
         } catch (LockedAccountException lae) {
-            System.out.println("用户名 " + token.getPrincipal() + " 被锁定 !");
+            logger.info("用户名 " + token.getPrincipal() + " 被锁定 !");
         }
         model.addAttribute("loginFail", "账号不存在或密码错误!");
         return "relogin";
