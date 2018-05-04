3a91b7834351cbe09b69c29649de7b841d0b0a7d
zjbman <zjbman@sina.com>
2018-05-05 00:56:11 +0800

    完成客户端的登录注册接口

--- src/main/java/com/paper/config/WebParam.java
+++ src/main/java/com/paper/config/WebParam.java
@@ -10,4 +10,17 @@ public class WebParam {
     public static final String LOGIN_USER = "loginUser";
     /** 登录用户权限 ,具体体现为登录用户所看到的菜单（子菜单）*/
     public static final String PERMISSION = "permission";
+
+
+    public static final int SUCCESS = 200;
+    public static final int FAIL = -100;
+
+    /** 登录返回参数*/
+    public static final int LOGIN_NOT_NULL = 101;
+    public static final int LOGIN_NOT_EXIST = 102;
+    public static final int LOGIN_NOT_RIGHT = 103;
+
+    /** 注册返回参数*/
+    public static final int REGISTER_ALREADY_EXIST = 104;
+
 }
---
+++ src/main/java/com/paper/controller/app/UserController.java
@@ -0,0 +1,100 @@
+package com.paper.controller.app;
+
+import com.paper.config.WebParam;
+import com.paper.entity.User;
+import com.paper.service.UserService;
+import com.paper.util.MD5;
+import com.paper.util.StringUtil;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 这是客户端 登录注册请求 的controller
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private Logger logger = Logger.getLogger(UserController.class.getSimpleName());
+
+    @Autowired
+    @Qualifier("userService")
+    UserService userService;
+
+    @RequestMapping("/Login")
+    public @ResponseBody
+    Map<String, Object> login(String username, String password) {
+        logger.info("成功进入登录接口");
+
+        Map<String, Object> result = new HashMap<String, Object>();
+        if (StringUtil.isEmpty(username) || StringUtil.isEmpty(password)) {
+            result.put("code", WebParam.LOGIN_NOT_NULL);
+            result.put("msg", "账号或密码不能为空");
+            return result;
+        }
+        User user = userService.findBySQL("select * from user where username = '" + username + "'", true);
+        if(user == null){
+            result.put("code", WebParam.LOGIN_NOT_EXIST);
+            result.put("msg", "账号或密码不正确");
+            return result;
+        }
+        if(!user.getPassword().equals(password)){
+            result.put("code", WebParam.LOGIN_NOT_RIGHT);
+            result.put("msg", "账号或密码不正确");
+            return result;
+        }
+        result.put("code", WebParam.SUCCESS);
+        result.put("msg", "登录成功");
+        return result;
+    }
+
+
+    @RequestMapping("/Register")
+    public @ResponseBody
+    Map<String,Object> register(String username,String password,String email,String qq,
+                                String telephone,String name){
+        logger.info("成功进入注册接口");
+        Map<String, Object> result = new HashMap<String, Object>();
+
+        User user = userService.findBySQL("select * from user where username = '" + username + "'", true);
+        if (user != null) {
+            result.put("code", WebParam.REGISTER_ALREADY_EXIST);
+            result.put("msg", "用户名已存在");
+            return result;
+        }
+
+        try {
+            User newUser = new User();
+            newUser.setUsername(username);
+            newUser.setPassword(MD5.md5(password));
+            newUser.setEmail(email);
+            newUser.setQq(qq);
+            newUser.setTelephone(telephone);
+            newUser.setName(name);
+            newUser.setState(0);
+
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            newUser.setCreateDate(format.format(new Date()));
+            newUser.setUpdateDate(format.format(new Date()));
+
+            userService.save(newUser);
+
+            result.put("code", WebParam.SUCCESS);
+            result.put("msg", "注册成功！");
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.put("code", WebParam.FAIL);
+            result.put("msg", "注册失败！");
+            return result;
+        }
+    }
+}
